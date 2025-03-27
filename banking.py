1ecbe8cd5f7f142b6a57c9329535883ca482862c
Ochieng' Meshack Otieno <146129953+Meshack12756@users.noreply.github.com>
2025-03-27 13:30:09 -0700

    Update banking.py

--- banking.py
+++ banking.py
@@ -1,5 +1,6 @@
-//This is a simple Python program that allows users to deposit money,
-withdraw funds, and check their account balance. 
-It provides a basic command-line interface for managing transactions securely and efficiently.
+## This is a simple Python program that allows users to deposit money,
+## withdraw funds, and check their account balance. 
+## It provides a basic command-line interface for managing transactions securely and efficiently.
 
-Users can deposit funds to increase their balance, withdraw money if they have sufficient funds, and check their current balance anytime. This mini-program is a great starting point for understanding banking transactions in Python
+## Users can deposit funds to increase their balance, withdraw money if they have sufficient funds, and check their current balance anytime. 
+## This mini-program is a great starting point for understanding banking transactions in Python 
